3ffdc14d1db9365c3477d2c08a409f666cf127c1
Tim Roeder <timothyjroeder@gmail.com>
2021-12-28 18:03:59 -0700

    Feat: add #avg_snack_price

--- app/models/machine.rb
+++ app/models/machine.rb
@@ -4,4 +4,11 @@ class Machine < ApplicationRecord
   belongs_to :owner
   has_many :machine_snacks
   has_many :snacks, through: :machine_snacks
+
+  # Class Methods
+
+  # Instance Methods
+  def avg_snack_price
+    snacks.average("price")
+  end
 end
